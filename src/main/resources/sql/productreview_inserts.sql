c0a0c2e094051c4cf710a833ab41b44cd8f64aa2
Ayşegül Arpacık <aysegul.arpacik@modanisa.com>
2021-12-21 00:48:16 +0300

    added sql inserts

---
+++ src/main/resources/sql/productreview_inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.PRODUCTREVIEW( ID, REVIEW, REVIEW_DATE, PRODUCT_ID, USER_ID) VALUES (    1    ,'Lorem ipsum 1',     now()    , 2,     2);
+INSERT INTO public.PRODUCTREVIEW( ID, REVIEW, REVIEW_DATE, PRODUCT_ID, USER_ID) VALUES (    2    ,'Lorem ipsum 2',     now()    , 4,     4);
+INSERT INTO public.PRODUCTREVIEW( ID, REVIEW, REVIEW_DATE, PRODUCT_ID, USER_ID) VALUES (    3    ,'Lorem ipsum 3',     now()    , 6,     6);
+INSERT INTO public.PRODUCTREVIEW( ID, REVIEW, REVIEW_DATE, PRODUCT_ID, USER_ID) VALUES (    4    ,'Lorem ipsum 4',     now()    , 1,     1);
+INSERT INTO public.PRODUCTREVIEW( ID, REVIEW, REVIEW_DATE, PRODUCT_ID, USER_ID) VALUES (    5    ,'Lorem ipsum 5',     now()    , 3,     3);
+INSERT INTO public.PRODUCTREVIEW( ID, REVIEW, REVIEW_DATE, PRODUCT_ID, USER_ID) VALUES (    6    ,'Lorem ipsum 6',     now()    , 4,     7);
+INSERT INTO public.PRODUCTREVIEW( ID, REVIEW, REVIEW_DATE, PRODUCT_ID, USER_ID) VALUES (    7    ,'Lorem ipsum 7',     now()    , 5,     5);
+INSERT INTO public.PRODUCTREVIEW( ID, REVIEW, REVIEW_DATE, PRODUCT_ID, USER_ID) VALUES (    8    ,'Lorem ipsum 8',     now()    , 5,     2);
---
+++ src/main/resources/sql/user_inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.USERS( ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, USERNAME) VALUES (    1    ,'Bilbo',    'Baggins'    , 'bilbo@gmail.com',     5558886611, 'bilbo');
+INSERT INTO public.USERS( ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, USERNAME) VALUES (    2    ,'Arthur',    'Dent'    , 'arthur@gmail.com',     5558886622, 'arthur');
+INSERT INTO public.USERS( ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, USERNAME) VALUES (    3    ,'Zaphod',    'Beeblebrox'    , 'arthur@gmail.com',     5558886633, 'zaphod');
+INSERT INTO public.USERS( ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, USERNAME) VALUES (    4    ,'Ford',    'Prefect'    , 'ford@gmail.com',     5558886644, 'ford');
+INSERT INTO public.USERS( ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, USERNAME) VALUES (    5    ,'Marvin',    'Robot'    , 'marvin@gmail.com',     5558886655, 'marvin');
+INSERT INTO public.USERS( ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, USERNAME) VALUES (    6    ,'Hermony',    'Granger'    , 'hermony@gmail.com',     5558887766, 'hermony');
+INSERT INTO public.USERS( ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, USERNAME) VALUES (    7    ,'Ron',    'Weasley'    , 'ron@gmail.com',     5558887777, 'ron');
+INSERT INTO public.USERS( ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, USERNAME) VALUES (    8    ,'Harry',    'Potter'    , 'harry@gmail.com',     5558887788, 'harry');
